9e79aaa1c8384f14370d74442d5f79d80d1f9293
liuji <1546243245@qq.com>
2024-05-15 15:42:14 +0800

    fix: use gadget from farm

--- Attack/target1/rtarget_level_2.s
+++ Attack/target1/rtarget_level_2.s
@@ -1,4 +1,4 @@
 address of touch2 0x4017ec
-address of gadget_2:  ret 0x40141c
+address of gadget_2: movq %rax, %rdi; ret;0x4019a2
 cookie  0x59b997fa
-address of gadget_1: pop %rdi ret 0x40141b
+address of gadget_1: popq %rax;no op; ret; 0x4019ab
